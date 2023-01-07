87e6a756a700c368786ec5271283b9a81ba4f258
one <i@uue.me>
2023-01-07 12:19:06 +0800

    相关功能按钮添加ToolTip

--- src/components/HomeMagicButton.vue
+++ src/components/HomeMagicButton.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { NButton, NPopover, NSpace } from "naive-ui";
+import type { StyleValue } from "vue";
+import { NButton, NPopover, NSpace, NTooltip } from "naive-ui";
 import { store } from "@/store";
+
+const itemTooltip: StyleValue = {
+  marginRight: "20px",
+  width: "max-content",
+};
 
 function backTop() {
   let el = document.querySelector(
@@ -26,29 +32,74 @@ function changeSiderVisible() {
 
 <template>
   <div class="more-btn">
-    <n-popover trigger="click">
+    <n-popover trigger="hover">
       <template #trigger>
         <n-button quaternary circle size="large">
-          <span class="item-icon">💭</span>
+          <span class="item-icon icon-swing">💭</span>
         </n-button>
       </template>
       <div>
         <n-space vertical>
-          <n-button quaternary circle size="large" @click="backTop">
-            <span class="item-icon icon-wobble-vertical">🚀</span>
-          </n-button>
-          <n-button quaternary circle size="large" @click="changeSiderVisible">
-            <span class="item-icon icon-swing">📜</span>
-          </n-button>
-          <n-button quaternary circle size="large" @click="changeTheme">
-            <span class="item-icon icon-rotateY" v-if="store.isDarkTheme"
-              >🧊</span
-            >
-            <span class="item-icon icon-flow" v-else>🌙</span>
-          </n-button>
-          <n-button quaternary circle size="large" @click="openSettingPage">
-            <span class="item-icon icon-rotate">⚙️</span>
-          </n-button>
+          <n-tooltip
+            :style="itemTooltip"
+            placement="left"
+            :show-arrow="false"
+            trigger="hover"
+          >
+            <template #trigger>
+              <n-button quaternary circle size="large" @click="backTop">
+                <span class="item-icon icon-wobble-vertical">🚀</span>
+              </n-button>
+            </template>
+            回到顶部
+          </n-tooltip>
+          <n-tooltip
+            :style="itemTooltip"
+            placement="left"
+            :show-arrow="false"
+            trigger="hover"
+          >
+            <template #trigger>
+              <n-button
+                quaternary
+                circle
+                size="large"
+                @click="changeSiderVisible"
+              >
+                <span class="item-icon icon-swing">📜</span>
+              </n-button>
+            </template>
+            侧栏
+          </n-tooltip>
+          <n-tooltip
+            :style="itemTooltip"
+            placement="left"
+            :show-arrow="false"
+            trigger="hover"
+          >
+            <template #trigger>
+              <n-button quaternary circle size="large" @click="changeTheme">
+                <span class="item-icon icon-rotateY" v-if="store.isDarkTheme"
+                  >🧊</span
+                >
+                <span class="item-icon icon-flow" v-else>🌙</span>
+              </n-button>
+            </template>
+            主题
+          </n-tooltip>
+          <n-tooltip
+            :style="itemTooltip"
+            placement="left"
+            :show-arrow="false"
+            trigger="hover"
+          >
+            <template #trigger>
+              <n-button quaternary circle size="large" @click="openSettingPage">
+                <span class="item-icon icon-rotate">⚙️</span>
+              </n-button>
+            </template>
+            设置
+          </n-tooltip>
         </n-space>
       </div>
     </n-popover>
